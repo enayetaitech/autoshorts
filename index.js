fa7a08cd84b96f615045ee0ce249bf41e92cebec
Enayet <enayetflweb@gamil.com>
2024-04-25 14:03:21 +0600

    for production

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 const PORT = process.env.PORT || 5000;
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
-const REDIRECT_URI = 'http://localhost:5000/oauth2callback';
+const REDIRECT_URI = 'https://autoshorts.onrender.com/oauth2callback';
 const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
 
 // Define the scope for YouTube video uploads
