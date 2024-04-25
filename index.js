cf9596845411d17dc32c76ca28c626c9e64b5851
Enayet <enayetflweb@gamil.com>
2024-04-25 13:02:39 +0600

    fake commit

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const { google } = require("googleapis");
 require("dotenv").config();
 fs = require("fs");
 const cors = require('cors');
-const ngrok = require('ngrok')
 
 const app = express();
 app.use(cors({
@@ -18,15 +17,15 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-app.use(express.json());
-app.use(bodyParser.json());
+// app.use(express.json());
+// app.use(bodyParser.json());
 
 const upload = multer({ dest: "./uploads/" });
 
 const PORT = process.env.PORT || 5000;
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
-const REDIRECT_URI = "http://localhost:5000/oauth2callback";
+const REDIRECT_URI = `http://localhost:${PORT}/oauth2callback`;
 const oAuth2Client = new google.auth.OAuth2(
   CLIENT_ID,
   CLIENT_SECRET,
@@ -36,19 +35,14 @@ const oAuth2Client = new google.auth.OAuth2(
 const SCOPES = ["https://www.googleapis.com/auth/youtube.upload"];
 
 
-app.post("/test", (req, res) => {
-  const testData = req.body;
-  console.log(testData)
 
-})
-
-app.get("/connect_youtube", (req, res) => {
+app.post("/connect_youtube", (req, res) => {
   const authUrl = oAuth2Client.generateAuthUrl({
     access_type: "offline",
     scope: SCOPES,
   });
   console.log('redirect url', authUrl)
-  res.redirect(`${ngrokInstance}/proxy${authUrl}`);
+  res.redirect(authUrl);
 });
 
 app.get("/oauth2callback", async (req, res) => {
@@ -56,7 +50,7 @@ app.get("/oauth2callback", async (req, res) => {
   const { tokens } = await oAuth2Client.getToken(code);
   console.log('token',tokens)
   oAuth2Client.setCredentials(tokens);
-  res.send("YouTube account connected. You can now upload videos.");
+  res.redirect("/upload_video");
 });
 
 // Web hook for sending data to the zapier
@@ -122,13 +116,12 @@ app.post("/upload_video", upload.single("video"), async (req, res) => {
   }
 });
 
-// (async function() {
-//   const ngrokInstance = await ngrok.connect({ port: 5000, addr: 'http://localhost:5000' });
-//   console.log(`Ngrok tunnel created: ${ngrokInstance}`);
-// })();
 
-app.listen(5000, () => {
-  console.log("Webhook endpoint listening on port 5000!");
+
+app.listen(PORT, () => {
+  console.log(`lOCAL HOST RUNNING ON: HTTP://LOCALHOST:${PORT}`);
+
+  
 });
 
 
