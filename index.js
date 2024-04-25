50e02c1eaef33f526049ac0c26af67f2eb752dcf
Enayet <enayetflweb@gamil.com>
2024-04-25 13:11:14 +0600

    for production

--- index.js
+++ index.js
@@ -1,39 +1,37 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const multer = require('multer');
+const { google } = require('googleapis');
+require('dotenv').config();
 
 const app = express();
-const upload = multer({ dest: './uploads/' });
+app.use(express.json());
 
+const PORT = process.env.PORT || 5000;
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = 'http://localhost:5000/oauth2callback';
+const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
 
-// Web hook for sending data to the zapier
-app.post('/webhook', upload.single('file'), (req, res) => {
-  const videoDetails = req.file;
-  // Send video details to Zapier
-  console.log('route hit')
-  console.log(videoDetails)
-  const zapierUrl = 'https://hooks.zapier.com/hooks/catch/18661439/372i6xc';
-  const options = {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(videoDetails),
-  };
-  fetch(zapierUrl, options)
-    .then((response) => response.json())
-    .then((data) => console.log('Zapier response:', data))
-    .catch((error) => console.error('Error sending data to Zapier:', error));
-    try {
-      res.send('Video details sent to Zapier!');
-    } catch (error) {
-      console.error('Error sending response:', error);
-      res.status(500).send('Error sending video details to Zapier');
-    }
+// Define the scope for YouTube video uploads
+const SCOPES = ['https://www.googleapis.com/auth/youtube.upload'];
+
+// Route to start the OAuth flow
+app.get('/connect_youtube', (req, res) => {
+    const authUrl = oAuth2Client.generateAuthUrl({
+        access_type: 'offline', // 'offline' access type is used to obtain a refresh token
+        scope: SCOPES, // Scopes define the level of access you need: in this case, to upload videos to YouTube
+    });
+    res.redirect(authUrl); // Redirect the user to the authentication URL
 });
 
-app.listen(5000, () => {
-  console.log('Webhook endpoint listening on port 5000!');
+// OAuth2 callback route
+app.get('/oauth2callback', async (req, res) => {
+    const { code } = req.query;
+    const { tokens } = await oAuth2Client.getToken(code);
+    oAuth2Client.setCredentials(tokens);
+    res.send('YouTube account connected. You can now upload videos.');
 });
 
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 // 1084926580778-s30h9vo5hvq7f5bn26uuujp7e2s9b20v.apps.googleusercontent.com
 
